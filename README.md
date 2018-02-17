479432168cfa3ed44fcc9660da03b5c571f126c8
Kmasteller <kennethmasteller@gmail.com>
2018-02-17 13:45:22 -0700

    skeleton setup with server

---
+++ README.md
@@ -0,0 +1,5 @@
+# REACT NYT SEARCH APP
+
+### By: Kenneth Masteller
+
+MERN stack react app for searching and storing articles from the NYT website. 
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Connect to the Mongo DB or localhost
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreactdb";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on('error',function(err){
+    console.log('Mongoose Error',err);
+});
+
+db.once('open', function(){
+    console.log("Mongoose connection is successful");
+});
+
+// Import the Article model
+const Article = require('./models/Article.js');
+
+// Import Routes
+require("./routes/apiRoutes")(app);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
