aa1dc4d2b7645fac606b90f2d88b98c78a6179ef
Kmasteller <kennethmasteller@gmail.com>
2018-02-21 19:30:08 -0700

    updated index.html & corrected controller

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -2,16 +2,16 @@ var Article = require("../models/Article");
 
 module.exports = {
     find: function(req, res) {
-        Article.find().then(function(document) {
-        res.json(document);
+        Article.find().then(function(data) {
+        res.json(data);
         }).catch(function(err) {
         res.json(err);
         });
     },
 
     insert: function(req, res) {
-        Article.create(req.body).then(function(document) {
-        res.json(document);
+        Article.create(req.body).then(function(data) {
+        res.json(data);
         }).catch(function(err) {
         res.json(err);
         });
@@ -20,8 +20,8 @@ module.exports = {
     delete: function(req, res) {
         Article.remove({
         _id: req.params.id
-        }).then(function(document) {
-        res.json(document);
+        }).then(function(data) {
+        res.json(data);
         }).catch(function(err) {
         res.json(err);
         });
