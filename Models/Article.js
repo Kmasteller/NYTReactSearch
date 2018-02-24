98847e98b442c1a849cb1de4274a758c1a762b71
kmasteller <kennethmasteller@gmail.com>
2018-02-24 15:42:44 -0700

    reverted article model

--- Models/Article.js
+++ Models/Article.js
@@ -1,42 +1,29 @@
-// // Require Mongoose
-// const mongoose = require('mongoose');
-// // Setup Schema
-// const Schema = mongoose.Schema;
-
-// const ArticleSchema = new Schema({
-
-//   title: {
-//     type: String,
-//     required: true
-//   },
-
-//   date: {
-//     type: String,
-//     required: true
-//   },
-
-//   link: {
-//     type: String,
-//     required: true
-//   }
-
-// });
-
-// // Create the Article model
-// const Article = mongoose.model('Article', ArticleSchema);
-
-// // Export
-// module.exports = Article;
-
-const mongoose = require("mongoose");
+// Require Mongoose
+const mongoose = require('mongoose');
+// Setup Schema
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-  title: { type: String, required: true },
-  link: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-}, { _id: false });
+const ArticleSchema = new Schema({
 
-const Article = mongoose.model("Article", articleSchema);
+  title: {
+    type: String,
+    required: true
+  },
 
+  date: {
+    type: String,
+    required: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  }
+
+});
+
+// Create the Article model
+const Article = mongoose.model('Article', ArticleSchema);
+
+// Export
 module.exports = Article;
